53c423ffd6f010a5fbbb83206aabdb2bc16fc544
Carlos-paez <carlospaezguerra@gmail.com>
2026-01-18 21:30:47 -0400

    boton de alerta

---
+++ Scripts/Darck_Mode.js
@@ -0,0 +1,6 @@
+const boton_1 = document.getElementById('alerta');
+const body_all = document.getElementById('body');
+
+boton_1.addEventListener('click', (e) => {
+    alert("Bienvenido a la página web de Monster Hunter");
+});
